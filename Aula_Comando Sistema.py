677ba53edd4e864c22fe2ed19704e94999dc13bc
ff2604 <ff2604@gmail.com>
2023-04-22 18:30:40 -0300

    Python exemplos

---
+++ Aula_Comando Sistema.py
@@ -0,0 +1,3 @@
+import os
+os.system('cls') # comando para widnows
+os.system('clear') # comando para linux
---
+++ Aula_comando de decisao.py
@@ -0,0 +1,41 @@
+
+aula="Curso"
+print(bool(aula))  #retorna true devido ter conteudo
+
+#aula=""
+#print(bool(aula))   #retorna false devido nao ter conteudo
+
+if aula:
+    print("Posssui texto" + " \"" + aula + "\"")
+else:
+    print("variavel aula esta vazia")
+
+print("===============================================================================")
+
+a=10
+b=5
+res=0
+op="-"
+
+if a>b:
+    print("Maior")
+else:
+    print("menor")
+
+print("--------------------------------------")
+
+if op == "+":
+    res=a+b
+elif op == "-":
+    res= a-b
+elif op == "*":
+    res= a*b
+elif op == "/":
+    res= a/b 
+
+if op != "+" and op != "-" and op != "*" and op != "/":
+    print("Operador invalido")
+else:
+    print(res)
+
+print("===============================================================================")
